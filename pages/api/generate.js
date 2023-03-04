2b1582869c378a404d6b4fa447bb1b8a43aa9596
Poupou <poupou-web3@protonmail.com>
2023-03-04 22:29:55 +0100

    Change image cache

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -10,6 +10,7 @@ const generateAction = async (req, res) => {
         headers: {
           Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
           'Content-Type': 'application/json',
+          'x-use-cache': 'false'
         },
         method: 'POST',
         body: JSON.stringify({
