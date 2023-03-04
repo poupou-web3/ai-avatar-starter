372373cf58539e2ff6b16ada6bf31acbe8cb8032
Poupou <poupou-web3@protonmail.com>
2023-03-04 21:28:53 +0100

    Add description and add prompt

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+
+  const [input, setInput] = useState('');
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -11,10 +19,13 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Mountain generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>This generates AI mountain pictures</h2>
+          </div>
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange} />
           </div>
         </div>
       </div>
